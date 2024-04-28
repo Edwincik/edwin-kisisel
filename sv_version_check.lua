9a1d7b21bc21d273e87deb01ea93fa8b7b6010d6
Nora Edwin <53962508+Edwincik@users.noreply.github.com>
2024-04-28 11:18:17 +0300

    Update sv_version_check.lua

--- sv_version_check.lua
+++ sv_version_check.lua
@@ -13,40 +13,30 @@ local label =
   ||                Created by Edwincik
   ||]]
 
--- Returns the current version set in fxmanifest.lua
 function GetCurrentVersion()
 	return GetResourceMetadata( GetCurrentResourceName(), "version" )
 end
 
--- Grabs the latest version number from the web GitHub
 PerformHttpRequest( "https://edwincik.github.io/edwincik-web/version.txt", function( err, text, headers )
-	-- Wait to reduce spam
 	Citizen.Wait( 2000 )
 
-	-- Print the branding!
 	print( label )
 
-	-- Get the current resource version
 	local curVer = GetCurrentVersion()
 
 	print( "  ||    Current version: " .. curVer )
 
 	if ( text ~= nil ) then
-		-- Print latest version
 		print( "  ||    Latest recommended version: " .. text .."\n  ||" )
 
-		-- If the versions are different, print it out
 		if ( text ~= curVer ) then
 			print( "  ||    ^1Your Edwin-Kisisel version is outdated, visit the FiveM forum post to get the latest version.\n^0  \\\\\n" )
 		else
 			print( "  ||    ^2Edwin-Kisisel is up to date!\n^0  ||\n  \\\\\n" )
 		end
 	else
-		-- In case the version can not be requested, print out an error message
 		print( "  ||    ^1There was an error getting the latest version information.\n^0  ||\n  \\\\\n" )
 	end
-
-	-- Warn the console if the resource has been renamed, as this will cause issues with the resource's functionality.
 	if ( GetCurrentResourceName() ~= "edwin-kisisel" ) then
 		print( "^1ERROR: Resource name is not edwin-kisisel, expect there to be issues with the resource. To ensure there are no issues, please leave the resource name as edwin-kisiel^0\n\n" )
 	end
